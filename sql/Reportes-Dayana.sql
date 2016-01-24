125fc9df2726ba25ddcdb2c5cc1c8467e8b918cf
David Garcia <david.a.g.r93@gmail.com>
2016-01-24 00:11:49 -0430

    27 Reportes terminados, faltan 2

--- sql/Reportes-Dayana.sql
+++ sql/Reportes-Dayana.sql
@@ -27,20 +27,68 @@ from caej ca,ejemplar,carrera
 where ca.fk_carr=carr_codigo and ca.fk_ejem=ejem_codigo and caej_posicion=1 and carr_categoria='clasico'
 order by ejemplar;
 
-%Listado de ejemplares ganadores de las ultimas 15 carreras del programa, agrupadas por tipo de carrera.%
+%Revisar-LISTO-Listado de ejemplares ganadores de las ultimas 15 carreras del programa, agrupadas por tipo de carrera. %
 select c.CARR_CODIGO,e.ejem_nombre ejemplar,c.carr_nombre carrera,CARR_TIPO tipo
 from ejemplar e,carrera c, caej ce 
 where ce.caej_posicion=1 and ce.fk_ejem=e.EJEM_CODIGO and  ce.fk_carr=c.carr_codigo and ROWNUM <=15
 group by carr_tipo,ejem_nombre,carr_nombre,carr_codigo
 order by carr_codigo desc; 
 
-%Listados de implementos.%
+%LISTO-Listados de implementos.%
 SELECT impl_nombre Implemento, impl_tipo Iniciales from implemento;
 
-%Listado de Entrenadores, indicando su cuadra.%
+%LISTO-Listado de Entrenadores, indicando su cuadra.%
 select e.entr_nombre ||' '|| entr_apellido entrenador,  c.caba_numero cuadra
 from entrenador e , caballeriza c 
-WHERE C.FK_ENTR = e.entr_codigo ;
+WHERE C.FK_ENTR = e.entr_codigo 
+order by entrenador;
 
-%Listado de Jinete%
-select j.jine_nombre ||' '|| jine_apellido Jinestes from jinete j;
+%LISTO-Listado de Jinete%
+select j.jine_nombre ||' '|| jine_apellido Jinetes from jinete j order by jinetes;
+
+--Historia de jinete con que ejemplar a ganado y cual carrera 
+select J.jine_nombre||' '||jine_apellido jinete,c.carr_nombre carrera, e.EJEM_NOMBRE ejemplar
+from jinete j ,ejemplar e,carrera c, caej ce 
+where  ce.caej_posicion=1 and ce.fk_ejem=e.EJEM_CODIGO and  ce.fk_carr=c.carr_codigo and ce.fk_jine=j.jine_codigo 
+order by jinete;
+
+-- Estadistica del ejemplar combinado jinete y entrenador 
+SELECT EJ.EJEM_NOMBRE ,CA.CAEJ_POSICION ,J.JINE_NOMBRE,E.ENTR_NOMBRE , CAR.CARR_NOMBRE
+FROM  ENTRENADOR E , JINETE J , CAEJ CA, CARRERA CAR, EJEMPLAR EJ, CABALLERIZA CAB , PUESTO PU
+WHERE CA.FK_EJEM=EJ.EJEM_CODIGO  AND CA.FK_CARR=CAR.CARR_CODIGO 
+AND CA.FK_JINE=J.JINE_CODIGO AND CAB.FK_ENTR=E.ENTR_CODIGO AND PU.FK_CABA=CAB.CABA_CODIGO AND EJ.FK_PUES=PU.PUES_CODIGO;
+
+select E.ejem_nombre Ejemplar,
+(select count(caej_posicion) from caej where fk_ejem=E.ejem_codigo and caej_posicion between 1 and 3) as GANADAS,
+(select count(caej_posicion) from caej where fk_ejem=E.ejem_codigo and caej_posicion > 3) as PERDIDAS,
+jine_nombre ||' '|| jine_apellido jinete,entr_nombre ||' '|| entr_apellido entrenador
+from ejemplar E, jinete j, entrenador e, caej ca, caballeriza c, puesto p
+where ejem_codigo=ca.fk_ejem and ca.fk_jine=jine_codigo and E.fk_pues=pues_codigo and p.fk_caba=caba_codigo and c.fk_entr=entr_codigo
+order by ejem_nombre;
+
+--Total de apuestas por taquilla por tipo de apuestas 
+select taqu_codigo taquilla, SUM(ta.TAAP_PRECIO) total, apue_nombre apuesta
+from TAQUILLA T, taap ta, apuesta a
+where T.TAQU_tipo='apuesta' and ta.fk_taqu=taqu_codigo and ta.fk_apue=apue_codigo
+group by taqu_codigo, apue_nombre;
+
+--total pagado por taquilla por carrera y por tipo de apuestas 
+select apue_nombre apuesta, taqu_codigo taquilla, carr_nombre carrera, SUM(c.caap_monto) monto
+from caap c , taap t , taquilla ta, carrera car , caej  cae, apuesta a
+where cae.fk_carr=carr_codigo and c.FK_CAEJ=cae.CAEJ_CODIGO and c.fk_taap=t.taap_codigo 
+and ta.TAQU_tipo='apuesta' and t.fk_taqu=taqu_codigo and t.fk_apue=apue_codigo
+group by taqu_codigo, carr_nombre, apue_nombre
+order by apuesta;
+
+--Indicar cuales son los implementos mas utilizados en las carreras de mayor a menos  segun su porcentaje de uso 
+select to_char((count(I.fk_impl)/(select count(fk_impl) from imca, implemento where fk_impl=impl_codigo)) * 100,'999.99') porcentaje, IMPL_tipo implemento
+from IMPLEMENTO IM, IMCA I, CAEJ CJ, CARRERA C
+where IM.IMPL_codigo=I.FK_IMPL and I.FK_CAEJ=CJ.CAEJ_codigo and CJ.FK_CARR=C.CARR_codigo and impl_tipo is not null
+group by impl_tipo;
+
+-- el promedio de ejemplares que corrieron en las ultimas 50 carreras segun su pelaje 
+select to_char(count(E.ejem_pelaje)/(select count(*) from EJEMPLAR E, CAEJ CJ, CARRERA CARR where E.EJEM_codigo=CJ.FK_EJEM and CJ.FK_CARR=CARR_codigo and carr_nombre=C.carr_nombre),'999.99') promedio, E.ejem_pelaje pelaje, carr_nombre carrera
+from EJEMPLAR E, CAEJ CJ, CARRERA C
+where E.EJEM_codigo=CJ.FK_EJEM and CJ.FK_CARR=C.CARR_codigo
+group by E.ejem_pelaje, carr_nombre, carr_codigo
+order by carr_codigo;
--- sql/ReportesSanti.sql
+++ sql/ReportesSanti.sql
@@ -16,7 +16,6 @@ select J.JINE_nombre,
 from JINETE J
 order by JINE_nombre;
 
-
 /*PROMEDIO DEL USO DE LOS IMPLEMENTOS EN LAS ULTIMAS 25 CARRERAS*/
 select carr_codigo,IMPL_tipo implemento, carr_nombre carrera, avg(I.fk_impl) promedio
 from IMPLEMENTO IM, IMCA I, CAEJ CJ, CARRERA C
@@ -24,28 +23,16 @@ where IM.IMPL_codigo=I.FK_IMPL and I.FK_CAEJ=CJ.CAEJ_codigo and CJ.FK_CARR=C.CAR
 group by impl_tipo, carr_nombre, carr_codigo
 order by carr_codigo desc;
 
-
 /*INDICAR CUALES SON LAS CARRERAS MAS FRECUENTES SEGUN SU TIPO*/
-
-
-
-
+select count(carr_tipo) frecuencia, carr_tipo tipo
+from carrera, caej
+where carr_codigo=fk_carr
+group by carr_tipo
+order by frecuencia desc;
 
 /*EL PROMEDIO DE EJEMPLARES QUE CORRIERON EN LAS ULTIMAS 50 CARRERAS SEGUN SU SEXO */
-//HEMBRA-YEGUA
-select avg(E.EJEM_codigo)
+select to_char(count(E.ejem_sexo)/(select count(*) from EJEMPLAR E, CAEJ CJ, CARRERA CARR where E.EJEM_codigo=CJ.FK_EJEM and CJ.FK_CARR=CARR_codigo and carr_nombre=C.carr_nombre),'999.99') promedio, E.ejem_sexo sexo, carr_nombre carrera
 from EJEMPLAR E, CAEJ CJ, CARRERA C
-where E.EJEM_codigo=CJ.FK_EJEM and
-CJ.FK_CARR=C.CARR_codigo and
-E.EJEM_sexo='Y';
-
-
-//MACHO-CABALLO
-select avg(E.EJEM_codigo)
-from EJEMPLAR E, CAEJ CJ, CARRERA C
-where E.EJEM_codigo=CJ.FK_EJEM and
-CJ.FK_CARR=C.CARR_codigo and
-E.EJEM_sexo='C';
-
-
-
+where E.EJEM_codigo=CJ.FK_EJEM and CJ.FK_CARR=C.CARR_codigo
+group by E.ejem_sexo, carr_nombre, carr_codigo
+order by carr_codigo;
