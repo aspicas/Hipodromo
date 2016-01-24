26b5b283129506fe3f9a3afa072b62323b79d996
David Garcia <david.a.g.r93@gmail.com>
2016-01-24 01:58:46 -0430

    Arreglo de reportes, Ya estan montados en jasper los de Dayana y los de Santi, faltan los mios

--- sql/Reportes-Dayana.sql
+++ sql/Reportes-Dayana.sql
@@ -46,18 +46,13 @@ order by entrenador;
 %LISTO-Listado de Jinete%
 select j.jine_nombre ||' '|| jine_apellido Jinetes from jinete j order by jinetes;
 
---Historia de jinete con que ejemplar a ganado y cual carrera 
+--LISTO-Historia de jinete con que ejemplar a ganado y cual carrera 
 select J.jine_nombre||' '||jine_apellido jinete,c.carr_nombre carrera, e.EJEM_NOMBRE ejemplar
 from jinete j ,ejemplar e,carrera c, caej ce 
 where  ce.caej_posicion=1 and ce.fk_ejem=e.EJEM_CODIGO and  ce.fk_carr=c.carr_codigo and ce.fk_jine=j.jine_codigo 
 order by jinete;
 
--- Estadistica del ejemplar combinado jinete y entrenador 
-SELECT EJ.EJEM_NOMBRE ,CA.CAEJ_POSICION ,J.JINE_NOMBRE,E.ENTR_NOMBRE , CAR.CARR_NOMBRE
-FROM  ENTRENADOR E , JINETE J , CAEJ CA, CARRERA CAR, EJEMPLAR EJ, CABALLERIZA CAB , PUESTO PU
-WHERE CA.FK_EJEM=EJ.EJEM_CODIGO  AND CA.FK_CARR=CAR.CARR_CODIGO 
-AND CA.FK_JINE=J.JINE_CODIGO AND CAB.FK_ENTR=E.ENTR_CODIGO AND PU.FK_CABA=CAB.CABA_CODIGO AND EJ.FK_PUES=PU.PUES_CODIGO;
-
+--LISTO-Estadistica del ejemplar combinado jinete y entrenador 
 select E.ejem_nombre Ejemplar,
 (select count(caej_posicion) from caej where fk_ejem=E.ejem_codigo and caej_posicion between 1 and 3) as GANADAS,
 (select count(caej_posicion) from caej where fk_ejem=E.ejem_codigo and caej_posicion > 3) as PERDIDAS,
@@ -66,13 +61,13 @@ from ejemplar E, jinete j, entrenador e, caej ca, caballeriza c, puesto p
 where ejem_codigo=ca.fk_ejem and ca.fk_jine=jine_codigo and E.fk_pues=pues_codigo and p.fk_caba=caba_codigo and c.fk_entr=entr_codigo
 order by ejem_nombre;
 
---Total de apuestas por taquilla por tipo de apuestas 
+--LISTO-Total de apuestas por taquilla por tipo de apuestas 
 select taqu_codigo taquilla, SUM(ta.TAAP_PRECIO) total, apue_nombre apuesta
 from TAQUILLA T, taap ta, apuesta a
 where T.TAQU_tipo='apuesta' and ta.fk_taqu=taqu_codigo and ta.fk_apue=apue_codigo
 group by taqu_codigo, apue_nombre;
 
---total pagado por taquilla por carrera y por tipo de apuestas 
+--LISTO-total pagado por taquilla por carrera y por tipo de apuestas 
 select apue_nombre apuesta, taqu_codigo taquilla, carr_nombre carrera, SUM(c.caap_monto) monto
 from caap c , taap t , taquilla ta, carrera car , caej  cae, apuesta a
 where cae.fk_carr=carr_codigo and c.FK_CAEJ=cae.CAEJ_CODIGO and c.fk_taap=t.taap_codigo 
@@ -80,13 +75,13 @@ and ta.TAQU_tipo='apuesta' and t.fk_taqu=taqu_codigo and t.fk_apue=apue_codigo
 group by taqu_codigo, carr_nombre, apue_nombre
 order by apuesta;
 
---Indicar cuales son los implementos mas utilizados en las carreras de mayor a menos  segun su porcentaje de uso 
+--LISTO-Indicar cuales son los implementos mas utilizados en las carreras de mayor a menos  segun su porcentaje de uso 
 select to_char((count(I.fk_impl)/(select count(fk_impl) from imca, implemento where fk_impl=impl_codigo)) * 100,'999.99') porcentaje, IMPL_tipo implemento
 from IMPLEMENTO IM, IMCA I, CAEJ CJ, CARRERA C
 where IM.IMPL_codigo=I.FK_IMPL and I.FK_CAEJ=CJ.CAEJ_codigo and CJ.FK_CARR=C.CARR_codigo and impl_tipo is not null
 group by impl_tipo;
 
--- el promedio de ejemplares que corrieron en las ultimas 50 carreras segun su pelaje 
+--Revisar-LISTO-el promedio de ejemplares que corrieron en las ultimas 50 carreras segun su pelaje 
 select to_char(count(E.ejem_pelaje)/(select count(*) from EJEMPLAR E, CAEJ CJ, CARRERA CARR where E.EJEM_codigo=CJ.FK_EJEM and CJ.FK_CARR=CARR_codigo and carr_nombre=C.carr_nombre),'999.99') promedio, E.ejem_pelaje pelaje, carr_nombre carrera
 from EJEMPLAR E, CAEJ CJ, CARRERA C
 where E.EJEM_codigo=CJ.FK_EJEM and CJ.FK_CARR=C.CARR_codigo
--- sql/ReportesSanti.sql
+++ sql/ReportesSanti.sql
@@ -1,36 +1,36 @@
-/*TOTAL DE VENTAS EN ENTRADAS AL RECINTO*/
+/*LISTO-TOTAL DE VENTAS EN ENTRADAS AL RECINTO*/
 select SUM(A.arta_precio)
 from TAQUILLA T, arta A
 where T.TAQU_tipo='boleto' and A.fk_taqu=taqu_codigo;
 
-/*TOTAL DE APUESTAS POR TAQUILLA*/
-select count(TP.fk_taqu) apuestas, taqu_codigo taquilla
+/*LISTO-TOTAL DE APUESTAS POR TAQUILLA*/
+select count(TP.fk_taqu) apuestas, taqu_codigo taquilla, SUM(taap_precio) Monto
 from TAQUILLA T, taap TP
 where T.TAQU_tipo='apuesta' and TP.fk_taqu=taqu_codigo
 group by taqu_codigo;
 
-/*ESTADISTICAS DEL JINETE BASADO EN LA CANTIDAD DE CARRERAS CORRIDAS*/
-select J.JINE_nombre,
+/*LISTO-ESTADISTICAS DEL JINETE BASADO EN LA CANTIDAD DE CARRERAS CORRIDAS*/
+select J.JINE_nombre ||' '|| jine_apellido jinete,
 (select count(CAEJ_posicion) from CAEJ where FK_JINE=J.JINE_codigo and CAEJ_posicion between 1 and 3) as GANADA,
 (select count(CAEJ_posicion) from CAEJ where FK_JINE=J.JINE_codigo and CAEJ_posicion > 3) as PERDIDA
 from JINETE J
 order by JINE_nombre;
 
-/*PROMEDIO DEL USO DE LOS IMPLEMENTOS EN LAS ULTIMAS 25 CARRERAS*/
+/*LISTO-PROMEDIO DEL USO DE LOS IMPLEMENTOS EN LAS ULTIMAS 25 CARRERAS*/
 select carr_codigo,IMPL_tipo implemento, carr_nombre carrera, avg(I.fk_impl) promedio
 from IMPLEMENTO IM, IMCA I, CAEJ CJ, CARRERA C
 where IM.IMPL_codigo=I.FK_IMPL and I.FK_CAEJ=CJ.CAEJ_codigo and CJ.FK_CARR=C.CARR_codigo and impl_tipo is not null
 group by impl_tipo, carr_nombre, carr_codigo
 order by carr_codigo desc;
 
-/*INDICAR CUALES SON LAS CARRERAS MAS FRECUENTES SEGUN SU TIPO*/
+/*LISTO-INDICAR CUALES SON LAS CARRERAS MAS FRECUENTES SEGUN SU TIPO*/
 select count(carr_tipo) frecuencia, carr_tipo tipo
 from carrera, caej
 where carr_codigo=fk_carr
 group by carr_tipo
 order by frecuencia desc;
 
-/*EL PROMEDIO DE EJEMPLARES QUE CORRIERON EN LAS ULTIMAS 50 CARRERAS SEGUN SU SEXO */
+/*LISTO-EL PROMEDIO DE EJEMPLARES QUE CORRIERON EN LAS ULTIMAS 50 CARRERAS SEGUN SU SEXO */
 select to_char(count(E.ejem_sexo)/(select count(*) from EJEMPLAR E, CAEJ CJ, CARRERA CARR where E.EJEM_codigo=CJ.FK_EJEM and CJ.FK_CARR=CARR_codigo and carr_nombre=C.carr_nombre),'999.99') promedio, E.ejem_sexo sexo, carr_nombre carrera
 from EJEMPLAR E, CAEJ CJ, CARRERA C
 where E.EJEM_codigo=CJ.FK_EJEM and CJ.FK_CARR=C.CARR_codigo
