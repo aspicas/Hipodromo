ad8106870f06b8a47f6c555b85a72357dd6bac16
Santiago <santimiguel2000@hotmail.com>
2016-01-25 10:51:28 -0430

    Vista Roles

--- Croles.php
+++ Croles.php
@@ -1,13 +1,14 @@
+
 <!DOCTYPE>
 <html>
 <head>
-<title>Hipodromo la Rinconada</title>
+	<title>Hipodromo la Rinconada</title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" width="device-width,initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.min.css">
-	<link rel="stylesheet" type="text/css" href="css/index_style.css">
+	<link rel="stylesheet" type="text/css" href="css/registro.css">	
 	<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
 	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
 </head>
@@ -15,104 +16,84 @@
 	
 	<?php include "navbar.php" ?>
 
-<div class="container space">	
-
-	<table class="table table-striped table-hover ">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>Usuario</th>
-      <th>Permisos</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr class="active">
-      <td>1</td>
-      <td>Admin</td>
-      <td><select name="codigo" required>
+	<div class="container">				
+		<form class="form-horizontal" method="post" action="php/CRUDTelefono.php">
+			<div class="form-group">
+				<label class="col-sm-offset-3 col-sm-9">Roles</label>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Opciones</label>
+					<div class="col-sm-9">
+						<select name="opcion" id="opcion" required>
+							<option>Seleccionar</option>
+							<option value="crear">Crear</option>
+							<option value="modificar">Modificar</option>
+							<option value="eliminar">Eliminar</option>
+						</select>
+					</div>				
+				</div>
+				<label for="haras" class="eliminar modificar col-sm-3 control-label">Rol actual</label>
+				<div class="col-sm-9">
+					<select class="eliminar modificar req" name="codigo" required>
+						<option>Seleccionar</option>
 						
-						
-		</select></td>
-    </tr>
-    <tr class="active">
-      <td>2</td>
-      <td>Propietario</td>
-      <td><select name="codigo" required>
-						
-						
-			</select></td>
-    </tr>
-    <tr class="active">
-      <td>3</td>
-      <td>Jinete</td>
-      <td><select name="codigo" required>
-						
-						
-			</select></td>
-    </tr>
-    <tr class="active">
-      <td>4</td>
-      <td>Veterinario</td>
-      <td><select name="codigo" required>
-						
-						
-			</select></td>
-    </tr>
-    <tr class="active">
-      <td>5</td>
-      <td>Entrenador</td>
-      <td><select name="codigo" required>
-						
-						
-			</select></td>
-    </tr>
-    <tr class="active">
-      <td>6</td>
-      <td>Invitado</td>
-      <td><select name="codigo" required>
-						
-						
-			</select></td>
-    </tr>
-  </tbody>
-</table> 
-
-<div class="form-group">
+					</select>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="crear col-sm-3 control-label">Rol</label>
+				<div class="col-sm-9">
+					<input class="crear req" type="text" class="form-control" name="telefono" required>
+				</div>
+				<br>
+				<label class="crear modificar col-sm-3 control-label" name="telefono">Permiso</label>
+				<select class="crear modificar req" name="codigo" required>
+							<option>Seleccionar</option>
+							
+						</select>
+			</div>			
+			<div class="form-group">
 				<div class="col-sm-offset-3 col-sm-9">
 					<button type="submit" class="btn btn-default" name="realizar">Realizar</button>
 				</div>
 			</div>
-
-</div>
-
-
-
-
-	<?php 
-		if (function_exists('foo')) { 
-		  print "foo defined\\n"; 
-		} else { 
-		  print "foo not defined\\n"; 
-		} 
-		function foo() {} 
-
-		if (function_exists('bar')) { 
-		  print "bar defined\\n"; 
-		} else { 
-		  print "defining bar\\n"; 
-		  function bar() {} 
-		} 
-		print "calling bar\\n"; 
-		bar(); // ok to call function conditionally defined earlier 
-
-		print "calling baz\\n"; 
-		baz(); // ok to call function unconditionally defined later 
-		function baz() {} 
-
-		qux(); // NOT ok to call function conditionally defined later 
-		if (!function_exists('qux')) { 
-		  function qux() {} 
-		} 
-	?> 
+		</form>
+	</div>
+	<script>
+		$(document).ready(function () {
+			$(".crear").hide();
+			$(".modificar").hide();
+			$(".eliminar").hide();
+			$(".req").removeAttr("required");
+			$('#opcion').on('change', function() {
+			  if (this.value == "crear") {			  	
+			  	$(".modificar").hide();
+			  	$(".eliminar").hide();
+			  	$(".req").removeAttr("required");
+			  	$(".crear").show();
+			  	$(".crear").attr("required", "required");
+			  }
+			  else if (this.value == "modificar"){
+			  	$(".crear").hide();
+			  	$(".eliminar").hide();
+			  	$(".req").removeAttr("required");
+			  	$(".modificar").show();
+			  	$(".modificar").attr("required", "required");
+			  }
+			  else if (this.value == "eliminar") {
+			  	$(".crear").hide();
+				$(".modificar").hide();
+				$(".eliminar").show();
+				$(".req").removeAttr("required");
+				$(".eliminar").attr("required", "required");
+			  }
+			  else {
+			  	$(".crear").hide();
+				$(".modificar").hide();
+				$(".eliminar").hide();
+				$(".req").removeAttr("required");
+			  };			  
+			});
+		});
+	</script>
 </body>
 </html>
